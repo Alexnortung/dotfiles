53cd57472033afe7b94ffce1efde487ab7607803
Alexander Nortung <alex_nortung@live.dk>
2019-11-05 08:54:54 +0100

    makes sure it does not link directories twice

--- create-links.sh
+++ create-links.sh
@@ -6,7 +6,7 @@ mkdir -p ~/.config/nvim/
 function ifNotExists() {
     FILENAME=$1;
     FILEDEST=$2;
-    if [ -f "$FILEDEST" ]; then
+    if [ -f "$FILEDEST" ] || [ -d "$FILEDEST" ]; then
         echo "$FILENAME already exists"; 
     else
         #link file here
